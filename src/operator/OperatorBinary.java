10b7491fcc026ba94720d5260950d60bc8c0f26a
Ariva2 <ariva080503@gmail.com>
2023-05-05 15:03:00 +0700

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,12 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assignment/penugasan
+    //menempatkan nilai kedalam variable
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Muhammad Ariva";
+    int julahsks = 24;
+    double indeksPresentasiKumilatif = 3.89;
+}
